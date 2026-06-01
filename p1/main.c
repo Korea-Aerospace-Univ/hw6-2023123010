cec21961f4dab03506ba9adff0a13b22768f550f
2023123010 <seonghyeon500@kau.kr>
2026-06-01 14:52:45 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char arr[10];
+    char *p;
+    char *q;
+    char max;
+    int m_count = 0;
+    
+    for (p = arr; p < arr + 10; p++) {
+        scanf("%c", p);
+    }
+    
+    for (p = arr; p < arr + 10; p++) {
+        int count = 0;
+        for (q = arr; q < arr + 10; q++) {
+            if (*p == *q) {
+                count++;
+            }
+        }
+        if (count > m_count) {
+        m_count = count;
+        max = *p;
+    }
+    }
+    printf("%c %d", max, m_count);
+}
